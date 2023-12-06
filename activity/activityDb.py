1d5dcec1dc54f28acafa6ffd4891cb76baf64aba
Frankanator8 <pika.and.scratch@gmail.com>
2023-12-05 23:57:13 -0500

    bug fix

--- activity/activityDb.py
+++ activity/activityDb.py
@@ -5,8 +5,8 @@ from activity.pointscalc import add_progress_points
 def add_points_overall(user, points):
     userData = database.activity.find_one(id=user)
     activity, progress = add_progress_points(userData["activity"], userData["progress"], points)
-    userData["progress"] = activity
-    userData["activity"] = progress
+    userData["progress"] = progress
+    userData["activity"] = activity
 
     database.activity.update_data("progress", userData["progress"], id=user)
     database.activity.update_data("activity", userData["activity"], id=user)
@@ -14,8 +14,8 @@ def add_points_overall(user, points):
 def add_points_weekly(user, points):
     userData = database.weekActivity.find_one(id=user)
     activity, progress = add_progress_points(userData["activity"], userData["progress"], points)
-    userData["progress"] = activity
-    userData["activity"] = progress
+    userData["progress"] = progress
+    userData["activity"] = activity
 
     database.weekActivity.update_data("progress", userData["progress"], id=user)
     database.weekActivity.update_data("activity", userData["activity"], id=user)
